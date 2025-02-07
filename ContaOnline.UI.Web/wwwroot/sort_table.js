c35c3bb404d3e254d0d6a340e6168a6a07394cde
Mauricio Sartori Mota <mauricio.08.04@gmail.com>
2025-02-07 20:35:47 -0300

    Creating sort feature in bills table

---
+++ ContaOnline.UI.Web/wwwroot/sort_table.js
@@ -0,0 +1,58 @@
+﻿var table = document.querySelector('.table.table-sort');
+
+if (table) {
+    let thead_cells = table.querySelectorAll('thead>tr>*');
+
+    if (thead_cells) {
+        thead_cells = Array.from(thead_cells);
+
+        thead_cells.forEach((thead_cell, index) => {
+            if (thead_cell.classList.value == 'table-head-command') return;
+
+            thead_cell.addEventListener('click', function () {
+
+                let isChanged = 0;
+
+                thead_cells.forEach(thead_cell_undecorate => {
+                    if (thead_cell_undecorate.classList.value === 'asc') {
+                        thead_cell_undecorate.classList[0] = '';
+                        thead_cell_undecorate.classList.value = '';
+                        isChanged = 1;
+                    }
+                });
+                
+                if (isChanged != 1) this.classList.toggle('asc');
+
+                sortTableByColumn(table, index, !this.classList.contains('asc'));
+            });
+        });
+    }
+}
+
+function sortTableByColumn(table, column, desc=false)
+{
+    let tbody = table.querySelector('tbody');
+        rows = tbody.querySelectorAll('tr');
+
+    rows = rows.isArray ? rows : Object.values(rows);
+
+    function compare(a, b) {
+        let aText = a.children[column].innerText.trim();
+            bText = b.children[column].innerText.trim();
+
+        return (aText < bText) ? -1 : 1;
+    }
+
+    rows.sort(compare);
+
+    if (desc) rows.reverse();
+
+    tbody.innerText = '';
+
+    let i = 0;
+    while (i < rows.length) {
+        tbody.append(rows[i]);
+        i++;
+    }
+
+}
